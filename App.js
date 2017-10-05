692b4e945edf2c8a085ff4987235da1a67d11e1a
Steven Starnes <starnes81@gmail.com>
2017-10-05 15:49:49 -0500

    imported react and created function

--- App.js
+++ App.js
@@ -3,3 +3,15 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+// import library to help create Component
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+//  create a Component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+// render to device

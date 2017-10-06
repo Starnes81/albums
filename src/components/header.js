2b0ebd70ae1abc503845ba43831c79d6114d962e
Steven Starnes <starnes81@gmail.com>
2017-10-06 11:26:36 -0500

    added src/component folder, header.js, and code for header

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+// import libraries for Component
+import React from 'react';
+import { Text } from 'react-native';
+// make a Component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+// export the Component
+export default Header;

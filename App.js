f28312359b0969e4f7644d1a287baad7bf780db1
Steven Starnes <starnes81@gmail.com>
2017-10-06 13:07:13 -0500

    created albumlist component

--- App.js
+++ App.js
@@ -5,13 +5,17 @@
  */
 // import library to help create Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 
 //  create a Component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
 
 // render to device
